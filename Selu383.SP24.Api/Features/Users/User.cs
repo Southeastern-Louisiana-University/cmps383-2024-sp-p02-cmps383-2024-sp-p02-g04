ed42d15bb90e579dcdc8f1b6b02cb6eb5fd31de0
Zachary Duncan <zachary.duncan@selu.edu>
2024-02-07 12:20:11 -0600

    Made UserDto, LoginDto, CreatedUserDto, and Edited HotelDto to reflect the addition of a manager.

---
+++ Selu383.SP24.Api/Features/Users/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Selu383.SP24.Api.Features.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
