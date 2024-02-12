4d785d744b36dcf743b2912446fda854dbea659b
Matthew Rendall <matthew.rendall@selu.edu>
2024-02-12 16:35:22 -0600

    deleted unnecessary files ya heard me

--- Selu383.SP24.Api/Data/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Selu383.SP24.Api.Data
-{
-    public class UserRole : IdentityUserRole<int>
-    {
-    }
-}
--- Selu383.SP24.Api/Role.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-public class Role : IdentityRole<int>
-{
-
-}
